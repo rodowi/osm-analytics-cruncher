4d5b9443168d210d949ccbc5910638132aa93856
Martin Raifer <tyr.asd@gmail.com>
2016-03-18 14:31:05 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict';
+var tileReduce = require('tile-reduce');
+var path = require('path');
+
+var mbtilesPath = process.argv[2] || "osm.mbtiles";
+
+tileReduce({
+    map: path.join(__dirname, '/map.js'),
+    sources: [{
+        name: 'osm',
+        mbtiles: mbtilesPath,
+        raw: false
+    }]
+})
+.on('end', function() {
+});
